c8d26ce424242ad76df767b9b68fee7797814ada
Tiffecko <justacc742004@gmail.com>
2024-03-03 09:53:50 +0500

    5.1 - 5.4 done

--- demo/src/main/java/com/example/demo/controller/PostsViewController.java
+++ demo/src/main/java/com/example/demo/controller/PostsViewController.java
@@ -1,17 +1,25 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.PostService;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Controller
 public class PostsViewController {
+
+    @Autowired
+    private PostService postsService;
+
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public String list(Model model) {
         model.addAttribute("appName", "Сентябрь горит");
+        model.addAttribute("posts", postsService.listAllPosts());
+
         return "list";
     }
 
